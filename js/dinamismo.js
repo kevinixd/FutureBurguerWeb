5a5778136da236b5f50e280741bc1af8ed73f89b
ike2103 <52331231+ike2103@users.noreply.github.com>
2019-08-05 13:03:44 -0600

    mañana
    termino

--- js/dinamismo.js
+++ js/dinamismo.js
@@ -1,3 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    $("#btnIncrementar").click(function () {
+        if($('#txt1').val()<5){
+            btnIncrementarVal($('#txt1'));
+         
+        }
+    })
 
+    $("#btnDisminuir").click(function () {
+        if($('#txt1').val()>1){
+           
+            btnDisminuirVal($('#txt1'));
+        }    
+
+    })
+
+
+    function btnIncrementarVal(selector) {
+        var $Item = selector;
+        var $curVal = $Item.attr("value");
+        $Item.attr("value", parseInt($curVal) + 1);
+    }
+
+
+    
+    function btnDisminuirVal(selector) { 
+        var $Item = selector; 
+        var $curVal = $Item.attr("value"); 
+        $Item.attr("value", parseInt($curVal) - 1); 
+    }
+
+    
 });
+
+
